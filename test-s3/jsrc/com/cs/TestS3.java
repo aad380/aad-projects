7ace9276ffcdf0a5ba2e7a1aa1a1daabb056f730
Alexander Dudarenko <alexander.dudarenko@abakus.me>
2014-12-10 23:30:03 +0200

    test s3 started

---
+++ test-s3/jsrc/com/cs/TestS3.java
@@ -0,0 +1,21 @@
+package com.cs;
+
+/**
+ * Copyright (c) 2013-2014 Abakus, Inc. All rights reserved.
+ * User: Alexander Dudarenko
+ * Date: 2014-12-10
+ */
+
+/**
+ * 
+ */
+public class TestS3 {
+
+    public TestS3() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println ("Hello World!");
+    }
+
+}
